05f86dd7cadce34f2f1d2d195d2b503dcd22dd2d
Jose Ignacio Aviles <Joseignacio_a@yahoo.com>
2020-01-16 08:19:52 -0500

    Fin sección 3

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
+
+const APP_ROUTES: Routes = [
+  { path: 'portafolio', component: PortafolioComponent},
+  { path: 'about', component: AboutComponent },
+  { path: 'item', component: ItemComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'portafolio'}
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot( APP_ROUTES, { useHash: true } )
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+
+export class AppRoutingModule {
+
+}
